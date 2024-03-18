48bf85056bb2afc985d84ff1a588415ce837a913
IrsanWoy! <105915685+irsanwoy@users.noreply.github.com>
2024-03-18 21:08:33 +0700

    Update 1-grow.js

--- 1-grow.js
+++ 1-grow.js
@@ -1,12 +1,13 @@
-jumlahDataArray = function(dataArray){
-    let result = 1;
-    for ( let i = 0 ; i < dataArray.length; i++ ){
-        result = result * dataArray[i];
-    }
-    return result;
-}
+// function grow(x){
+//     let result = x[0];
+//     for(let i = 1; i < x.length; i++ ){
+//         result = result * x[i];
+//     }
+//     return result;
+// }
 
-myArray = [1,2,3];
+// dengan reduce 
+const grow = (x) => x.reduce((acc,curr) => acc * curr, 1);
 
 
-console.log(jumlahDataArray(myArray))
+console.log(grow([1,2,3,4]))
